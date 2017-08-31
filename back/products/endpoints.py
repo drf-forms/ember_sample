a091d0746c08bfbdd1701b336c51dc7931ddc2fa
Emmanuelle Delescolle <emma@levit.be>
2017-08-31 09:08:31 +0200

    :heavy_plus_sign: API for products application

---
+++ back/products/endpoints.py
@@ -0,0 +1,17 @@
+from drf_auto_endpoint.endpoints import Endpoint
+from drf_auto_endpoint.router import router, register
+
+from .models import Product, Category
+
+
+@register
+class ProductEndpoint(Endpoint):
+    model = Product
+
+    page_size = 10
+    list_display = ['picture', 'name', 'category', 'sale_type', 'price' ]
+    search_fields = ['name', ]
+    filter_fields = ['category_id', ]
+
+
+router.register(Category)
