d5ab365b6ba5d2c13e95dbca7ce5b07f4605635e
Emmanuelle Delescolle <emma@levit.be>
2017-08-31 09:02:27 +0200

    :heavy_plus_sign: accounting application + models + migrations

---
+++ back/accounting/models.py
@@ -0,0 +1,57 @@
+from functools import reduce
+
+from django.db import models
+
+from crm.models import Company
+from products.models import Product
+
+
+INVOICE_TYPES = (
+    ('ci', 'Client'),
+    ('si', 'Supplier'),
+)
+
+INVOICE_STATES = (
+    ('draft', 'Draft'),
+    ('open', 'Open'),
+    ('paid', 'Paid'),
+)
+
+
+class Invoice(models.Model):
+
+    date = models.DateField(auto_now_add=True)
+    invoice_type = models.CharField(max_length=2, choices=INVOICE_TYPES)
+    related_party = models.ForeignKey(Company, related_name='invoices')
+    state = models.CharField(max_length=5, choices=INVOICE_STATES)
+
+    @property
+    def sign(self):
+        if self.invoice_type == 'si':
+            return '-'
+        return ''
+
+    @property
+    def total(self):
+        rv = 0
+        for line in self.lines.all():
+            rv += line.total
+        return rv
+
+    def __str__(self):
+        return 'Invoice #{} - {} ({}{})'.format(self.id, self.related_party, self.sign, self.total)
+
+
+class InvoiceLine(models.Model):
+
+    invoice = models.ForeignKey(Invoice, related_name='lines')
+    product = models.ForeignKey(Product, related_name='+')
+    quantity = models.PositiveIntegerField()
+    position = models.PositiveSmallIntegerField(default=0)
+
+    @property
+    def total(self):
+        return self.quantity * self.product.price
+
+    def __str__(self):
+        return '{} X {}'.format(self.quantity, self.product.name)
--- back/settings.py
+++ back/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = (
     # My applications
     'crm',
     'products',
+    'accounting',
 
     # Tests
     # 'factory',
